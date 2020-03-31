bbabf356509942dbe43277eaf65bce577cd5eb70
Sujan Paudel <61535893+examfear7@users.noreply.github.com>
2020-03-31 13:26:44 +0545

    Add files via upload

---
+++ application/config/database.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$active_group = 'default';
+$query_builder = TRUE;
+
+$db['default'] = array(
+	'dsn'	=> '',
+	'hostname' => 'sql12.freemysqlhosting.net',
+	'username' => 'sql12330141',
+	'password' => 'my6YfuUPKu',
+	'database' => 'sql12330141',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8mb4',
+	'dbcollat' => 'utf8mb4_unicode_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
